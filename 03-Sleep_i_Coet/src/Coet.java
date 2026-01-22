b956e41de6cbe5bac79fc55523140b2639a36676
Set catalan <2024_set.catalan@iticbcn.cat>
2026-01-22 23:35:13 +0100

    Activitat acabada

--- 03-Sleep_i_Coet/src/Coet.java
+++ 03-Sleep_i_Coet/src/Coet.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+
 public class Coet {
-    
+
+    private static Motor motor0 = new Motor(0);
+    private static Motor motor1 = new Motor(1);
+    private static Motor motor2 = new Motor(2);
+    private static Motor motor3 = new Motor(3);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        motor0.start();
+        motor1.start();
+        motor2.start();
+        motor3.start();
+
+        int p = scanner.nextInt();
+        while (p != 0) {
+            passaAPotencia(p);
+            p = scanner.nextInt();
+        }
+        
+        passaAPotencia(p);
+        scanner.close();
+    }
+
+    public static void passaAPotencia(int p){
+        if (p > 10){
+            System.out.println("La potència no pot ser superior a 10");
+        } else if (p < 0){
+            System.out.println("La potència no pot ser menor que 0");
+        } else {
+            System.out.println("Passant a pòtencia " + p);
+            motor0.setPotencia(p);
+            motor1.setPotencia(p);
+            motor2.setPotencia(p);
+            motor3.setPotencia(p);
+        }
+    }
 }
--- 03-Sleep_i_Coet/src/Motor.java
+++ 03-Sleep_i_Coet/src/Motor.java
@@ -2,16 +2,20 @@ public class Motor extends Thread{
     
     private int pObjectiu;
     private int pActual;
+    private boolean actiu;
 
     public Motor(int motor){
         super("Motor " + motor);
         pObjectiu = 0;
         pActual = 0;
+        actiu = true;
     }
 
     public void setPotencia(int p){
         pObjectiu = p;
-        run();
+        if (p == 0){
+            actiu = false;
+        }
     }
 
     public void decrementaPotencia(){
@@ -25,44 +29,38 @@ public class Motor extends Thread{
             System.out.println(String.format("%s: Decre. Objectiu: %d Actual: %d", this.getName(), pObjectiu, pActual));
             pActual--;
         }
+        System.out.println(String.format("%s: FerRes Objectiu: %d Actual: %d", this.getName(), pObjectiu, pActual));
     }
 
     public void incrementaPotencia(){
         while (pActual < pObjectiu) {
             Long interval_aleatori = Math.round((Math.random() * 1000) + 1000);
+            System.out.println(String.format("%s: Incre. Objectiu: %d Actual: %d", this.getName(), pObjectiu, pActual));
+            pActual++;
             try {
                 sleep(interval_aleatori);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(String.format("%s: Incre. Objectiu: %d Actual: %d", this.getName(), pObjectiu, pActual));
-            pActual++;
-        }
-    }
-
-    @Override
-    public void run(){
-        if (pObjectiu < pActual){
-            pActual--;
-            decrementaPotencia();
-        } else if (pObjectiu > pActual){
-            pActual++;
-            incrementaPotencia();
         }
         System.out.println(String.format("%s: FerRes Objectiu: %d Actual: %d", this.getName(), pObjectiu, pActual));
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        Motor motor = new Motor(1);
-        Motor motor2 = new Motor(2);
-
-        motor.setPotencia(3);
-        motor2.setPotencia(3);
-
-        while (motor.isAlive() || motor2.isAlive()) {
+    @Override
+    public void run(){
+        while(actiu || pActual != pObjectiu){
+            if (pObjectiu < pActual){
+                pActual--;
+                decrementaPotencia();
+            } else if (pObjectiu > pActual){
+                pActual++;
+                incrementaPotencia();
+            }
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-
-        motor.setPotencia(0);
-        motor2.setPotencia(0);
     }
 }
